c4c7b229ef1536efa1e3ccd44000565f8023003f
jeff_simon <jeffsbillinfo@gmail.com>
2020-05-01 22:20:18 -0700

    Done.

--- index.js
+++ index.js
@@ -20,3 +20,77 @@ let allWagesFor = function () {
 
     return payable
 }
+
+function calculatePayroll(employees)
+{
+     function payroll(total, employee){
+         total = total + allWagesFor.call(employee);
+         return total
+     }
+     let payrollTotal = employees.reduce(payroll, 0);
+     console.log(payrollTotal)
+     return payrollTotal
+}
+
+// function calculatePayroll(employees)
+// {
+//      function payroll(total, employee){
+//          total = total + allWagesFor(employee);
+//          return total
+//      }
+//      let payrollTotal = employees.reduce(payroll, 0);
+//      console.log(payrollTotal)
+//      return payrollTotal
+// }
+
+function createEmployeeRecord( userData )
+{
+    return {firstName: userData[0], 
+        familyName: userData[1], 
+        title: userData[2], 
+        payPerHour: userData[3], 
+        timeInEvents: [], 
+        timeOutEvents: [] 
+        }
+}
+
+function createEmployeeRecords(employees)
+{
+    return employees.map(function(employee) { return new createEmployeeRecord(employee) } )    
+}
+
+let createTimeInEvent = function(dateStamp)
+{    
+    let parsedDate = dateStamp.split(" ");
+    let timeIn = {type: "TimeIn", hour: parseInt(parsedDate[1]), date: parsedDate[0]}
+    this.timeInEvents.push(timeIn);
+    return this
+}
+
+let createTimeOutEvent = function (dateStamp)
+{
+    //"YYYY-MM-DD 800"
+    let parsedDate = dateStamp.split(" ");
+    let timeOut = {type: "TimeOut", hour: parseInt(parsedDate[1]), date: parsedDate[0]}
+    this.timeOutEvents.push(timeOut);
+    return this
+}
+
+let hoursWorkedOnDate = function (dateStamp)
+{   
+    function dateToFind(date){ return date.date === dateStamp}
+    let parsedDate = dateStamp.split(" ");
+    let checkIn = this.timeInEvents.find(dateToFind);
+    let checkOut = this.timeOutEvents.find(dateToFind);    
+    return (checkOut.hour - checkIn.hour)/100
+}
+
+let wagesEarnedOnDate = function (dateStamp)
+{    
+    return this.payPerHour * hoursWorkedOnDate.call(this, dateStamp)
+}
+
+function findEmployeeByFirstName(srcArray, firstName)
+{
+    return srcArray.find(employee => employee.firstName === firstName);    
+}
