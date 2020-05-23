ef80f43f31e061741063d195543268baf4dcd383
github username <github email address>
2020-05-23 13:19:14 -0700

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,70 @@
 /* Your Code Here */
+function createEmployeeRecord([firstName, familyName, title, payPerHour]) {
+    return {
+        firstName: firstName, 
+        familyName: familyName,
+        title: title,
+        payPerHour: payPerHour,
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
 
+function createEmployeeRecords(arraryOfArrays) {
+    const arrayOfObjects = arraryOfArrays.map(employeeArray => 
+        createEmployeeRecord(employeeArray));
+    return arrayOfObjects
+}
+
+function createTimeInEvent(dateStamp) {
+    const d = dateStamp.split(/\s/);
+    const date = d[0];
+    const hour =parseInt(d[1]);
+        this.timeInEvents.push({
+        type: "TimeIn",
+        hour: hour,
+        date: date
+        })
+    return this
+}
+
+function createTimeOutEvent(dateStamp) {
+    const d = dateStamp.split(/\s/);
+    const date = d[0];
+    const hour =parseInt(d[1]);
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: hour,
+        date: date
+        })
+    return this
+}
+
+function hoursWorkedOnDate(d) {
+    const startTime = this.timeInEvents.find(event => event.date === d);
+    const endTime = this.timeOutEvents.find(event => event.date === d);
+    const hoursWorked = (endTime.hour - startTime.hour) / 100;
+    return hoursWorked
+}
+
+function wagesEarnedOnDate(d) {
+    const payOwed = parseInt(this.payPerHour) * hoursWorkedOnDate.call(this, d);
+    return payOwed
+}
+
+function findEmployeeByFirstName(srcArray, firstName) {
+    let employee;
+    employee = srcArray.find(employeeObject => employeeObject.firstName === firstName)
+    return employee
+}
+
+function calculatePayroll(employeesArray) {
+    function getSum(total, num) {
+        return total + num;
+      }
+    const sumOfPay = employeesArray.map(employee => allWagesFor.apply(employee))
+    return sumOfPay.reduce(getSum)
+}
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
